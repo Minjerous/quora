1f71e0b0b53a54a78f9f2d97a6f695cab2fd1c28
MinJerous <1725014728@qq.com>
2022-09-09 08:38:07 +0800

    feat:md

--- app/qa/cmd/rpc/internal/logic/addQuestionLogic.go
+++ app/qa/cmd/rpc/internal/logic/addQuestionLogic.go
@@ -2,11 +2,10 @@ package logic
 
 import (
 	"context"
-	"quora/app/qa/model"
-	"time"
-
 	"quora/app/qa/cmd/rpc/internal/svc"
 	"quora/app/qa/cmd/rpc/pb"
+	"quora/app/qa/newModel"
+	"time"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -26,18 +25,18 @@ func NewAddQuestionLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddQu
 }
 
 func (l *AddQuestionLogic) AddQuestion(in *pb.AddQuestionReq) (*pb.AddQuestionResp, error) {
-	RS, err := l.svcCtx.QuestionModel.Insert(l.ctx, &model.Question{
+	_, err := l.svcCtx.NewQuestionModel.Insert(l.ctx, &newModel.Question{
 		Uid:       in.Uid,
 		Topic:     in.Topic,
 		Title:     in.Title,
-		Value:     in.Title,
+		Value:     in.Value,
 		CreatedAt: time.Unix(in.CreatedAt, 0),
+		UpdateAt:  time.Unix(in.CreatedAt, 0),
 	})
-	Id, err := RS.LastInsertId()
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &pb.AddQuestionResp{Id: Id}, nil
+	return &pb.AddQuestionResp{}, nil
 }
